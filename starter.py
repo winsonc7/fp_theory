e8cda31ea2da031bd081ce060d70e716c1c48d28
winsonc7 <winsonc@stanford.edu>
2023-11-06 12:06:55 -0800

    debugged

--- starter.py
+++ starter.py
@@ -1,29 +1,33 @@
 import numpy as np
-from scipy.sparse import csr_matrix
-from scipy.sparse import eye
+from scipy.sparse import csr_matrix, eye, save_npz
 
-A = 2       # forward rate
-B = 1       # backward rate
-N = 5       # matrix size
+A = 4       # forward rate
+B = 2       # backward rate
+N = 10       # matrix size: assumes N > 1
 T = 1       # time
-K = 10      # num terms to sum
+K = 100      # num terms to sum: assumes K > 1
 
 
 def generate_rate_matrix(a, b, n):
-    rate_matrix = csr_matrix((n,n), dtype=float)
-    for i in range(n):
-        if i + 1 < n - 1:   # bounds checking
-            rate_matrix[i][i+1] = a
-        if i != 0:          # bounds checking
-            rate_matrix[i][i-1] = b
-        rate_matrix[i][i] = sum(rate_matrix[i]) * -1
+    data = [a*-1, a]            # first row
+    row_indices = [0, 0]
+    column_indices = [0, 1]
+    for i in range(1,n):
+        vals = []
+        if i < n - 1:
+            vals = [b, -1*(a+b), a]
+        else:
+            vals = [b, b*-1]    # last row
+        data = data + vals
+        row_indices = row_indices + [i for j in range(len(vals))]
+        column_indices = column_indices + [(i-1) + j for j in range(len(vals))]
+    rate_matrix = csr_matrix((data, (row_indices, column_indices)), shape=(n, n))
     return rate_matrix
 
 def factorial_memo(n):
     memo = [1]
     for i in range(1, n):
         memo.append(memo[-1] * i)
-    print("done")
     return memo
 
 def standard_exponential(q, t, k):
@@ -33,11 +37,9 @@ def standard_exponential(q, t, k):
     sum = curr_q
     for i in range(1, k):
         curr_q = curr_q @ new_q         # multiplication of sparse matrices
-        sum += new_q / f_memo[i]
-        print(i)
+        sum += curr_q / f_memo[i]
     return sum
 
-# q_matrix = generate_rate_matrix(A, B, N)
-# print("done")
-# standard_answer = standard_exponential(q_matrix, T, K)
-# print(standard_answer)
+q_matrix = generate_rate_matrix(A, B, N)
+standard_answer = standard_exponential(q_matrix, T, K)
+save_npz("pmatrix_standard.npz", standard_answer)
