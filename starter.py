e1b31a74e2f758cb5ffc76b64bd00c30e1f06450
winsonc7 <winsonc@stanford.edu>
2023-11-06 12:59:21 -0800

    wrote uniformization

--- starter.py
+++ starter.py
@@ -1,9 +1,10 @@
 import numpy as np
+import math
 from scipy.sparse import csr_matrix, eye, save_npz
 
 A = 4       # forward rate
 B = 2       # backward rate
-N = 10       # matrix size: assumes N > 1
+N = 3       # matrix size: assumes N > 1
 T = 1       # time
 K = 100      # num terms to sum: assumes K > 1
 
@@ -24,6 +25,16 @@ def generate_rate_matrix(a, b, n):
     rate_matrix = csr_matrix((data, (row_indices, column_indices)), shape=(n, n))
     return rate_matrix
 
+def generate_const_matrix(num, n):
+    data = [num for i in range(n**2)]
+    row_indices = []
+    column_indices = []
+    for i in range(n):
+        row_indices += [i for j in range(n)]
+        column_indices += [j for j in range(n)]
+    m = csr_matrix((data, (row_indices, column_indices)), shape=(n, n))
+    return m
+
 def factorial_memo(n):
     memo = [1]
     for i in range(1, n):
@@ -40,6 +51,18 @@ def standard_exponential(q, t, k):
         sum += curr_q / f_memo[i]
     return sum
 
+def unif_exponential(matrix, a, b, t, k):
+    larger = max(a, b)              # csr matrices can't directly add non-zero scalars
+    add_m = generate_const_matrix(larger, matrix.shape[0])
+    shifted_m = matrix + add_m
+    e_const = math.exp(-1*larger*t)
+    unif_m = standard_exponential(shifted_m, t, k) * e_const
+    return unif_m
+
 q_matrix = generate_rate_matrix(A, B, N)
-standard_answer = standard_exponential(q_matrix, T, K)
-save_npz("pmatrix_standard.npz", standard_answer)
+
+# standard_answer = standard_exponential(q_matrix, T, K)
+# np.savetxt("p_matrix_standard.csv", standard_answer.toarray(), delimiter=',', fmt='%f')
+
+unif_answer = unif_exponential(q_matrix, A, B, T, K)
+np.savetxt("p_matrix_unif.csv", unif_answer.toarray(), delimiter=',', fmt='%f')
