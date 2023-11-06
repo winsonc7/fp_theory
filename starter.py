c2b089f2175a4c9a52211e8399181cc49bca2ed5
winsonc7 <winsonc@stanford.edu>
2023-11-06 11:03:51 -0800

    built other functions

--- starter.py
+++ starter.py
@@ -1,11 +1,13 @@
 import numpy as np
 from scipy.sparse import csr_matrix
+from scipy.sparse import eye
 
-A = 5       # forward rate
-B = 2       # backward rate
-N = 1000    # matrix size
+A = 2       # forward rate
+B = 1       # backward rate
+N = 5       # matrix size
 T = 1       # time
-K = 1000    # num terms to sum
+K = 10      # num terms to sum
+
 
 def generate_rate_matrix(a, b, n):
     rate_matrix = csr_matrix((n,n), dtype=float)
@@ -17,14 +19,25 @@ def generate_rate_matrix(a, b, n):
         rate_matrix[i][i] = sum(rate_matrix[i]) * -1
     return rate_matrix
 
-def 
+def factorial_memo(n):
+    memo = [1]
+    for i in range(1, n):
+        memo.append(memo[-1] * i)
+    print("done")
+    return memo
 
+def standard_exponential(q, t, k):
+    f_memo = factorial_memo(k)      # calculates factorials beforehand, idk felt like it'd be faster
+    new_q = t * q
+    curr_q = eye(new_q.shape[0], format='csr')          # identity matrix
+    sum = curr_q
+    for i in range(1, k):
+        curr_q = curr_q @ new_q         # multiplication of sparse matrices
+        sum += new_q / f_memo[i]
+        print(i)
+    return sum
 
-
-
-
-
-
-r_matrix = generate_rate_matrix(A, B, N)
-
-
+# q_matrix = generate_rate_matrix(A, B, N)
+# print("done")
+# standard_answer = standard_exponential(q_matrix, T, K)
+# print(standard_answer)
